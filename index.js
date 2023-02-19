f9612e8dc5e5674c366d7c8d67abe49b01feb387
Github@20020725 <garvitchaurasiya725@gmail.com>
2023-02-19 14:57:48 +0530

    Refactored the code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const connectToMongo = require('./database/db');
 
 const express = require('express')
 var cors = require('cors')
-require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
--- routes/video.js
+++ routes/video.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const Video = require('../models/Video')
-const multer  = require('multer')
+const multer = require('multer')
 const fetchuser = require('../middleware/fetchuser');
 
 
@@ -86,49 +86,47 @@ router.get('/load/:filename', (req, res) => {
                     err: 'No file exists'
                 })
             }
-    
+
             const range = req.headers.range;
             if (!range) {
                 res.status(400).send("Requires Range header");
             }
-    
+
             const videoSize = file.length;
             const start = Number(range.replace(/\D/g, ""));
             const end = videoSize - 1;
-    
+
             // const CHUCK_SIZE = 10**7;
             // const videoSize = file.length;
             // const start = Number(range.replace(/\D/g, ""));
             // const end = Math.min(start + CHUCK_SIZE, videoSize - 1);
             const contentLength = end - start;
-    
-            console.log(start, end, contentLength, `bytes ${start}-${end}/${videoSize}`);
-    
+
             const headers = {
                 "Content-Range": `bytes ${start}-${end}/${videoSize}`,
                 "Accept-Ranges": "bytes",
                 "Content-Length": contentLength,
                 "Content-Type": "video/mp4"
             }
-    
+
             if (contentLength == 0) {
                 return res.status(200).send("Done");
             }
             res.writeHead(206, headers);
-    
+
             const downloadStream = gridfsBucket.openDownloadStreamByName(file.filename, {
                 start: start,
                 end: end
             })
             downloadStream.pipe(res);
-    
+
         })
     } catch (error) {
         console.log(error.message);
         return res.json(500).send(error.message);
     }
 
-    
+
 })
 
 
@@ -172,243 +170,259 @@ router.get('/load/:filename', (req, res) => {
 //     res.json({success:true, file: req.file});
 // })
 
-router.post('/like', fetchuser, async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
+router.post('/like', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
     try {
-        const {filename} = req.body;
-        await Video.findOneAndUpdate({filename: filename}, 
-            {$push: {
-                "likes": {username: req.user.username}
-            }});
-        const video = await Video.findOne({filename});
-        res.json({likes: video.likes});
+        const { filename } = req.body;
+        await Video.findOneAndUpdate({ filename: filename },
+            {
+                $push: {
+                    "likes": { username: req.user.username }
+                }
+            });
+        const video = await Video.findOne({ filename });
+        res.json({ likes: video.likes });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 })
 
-router.post('/dislike', fetchuser, async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
+router.post('/dislike', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
     try {
-        const {filename} = req.body;
-        await Video.findOneAndUpdate({filename: filename}, 
-            {$pull: {
-                "likes": {username: req.user.username}
-            }});
-        const video = await Video.findOne({filename});
-        res.json({likes: video.likes});
+        const { filename } = req.body;
+        await Video.findOneAndUpdate({ filename: filename },
+            {
+                $pull: {
+                    "likes": { username: req.user.username }
+                }
+            });
+        const video = await Video.findOne({ filename });
+        res.json({ likes: video.likes });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
-    }
-
-})
-
-
-router.post('/alreadyliked', fetchuser, async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
-    try {
-        const {filename} = req.body;
-        const isLiked = await Video.findOne({filename: filename, likes: {username:req.user.username} })
-        res.json({isLiked});
-
-    } catch (error) {
-        console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 
 })
 
-router.post('/getlikes', async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
-    try {
-        const {filename} = req.body;
-        
-        const video = await Video.findOne({filename});
 
-        res.json({totalLikes: video.likes.length});
+router.post('/alreadyliked', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    try {
+        const { filename } = req.body;
+        const isLiked = await Video.findOne({ filename: filename, likes: { username: req.user.username } })
+        res.json({ isLiked });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
-    
 
 })
 
-router.get('/getvideos', async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
-    let videos = await Video.find();
+router.post('/getlikes', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    try {
+        const { filename } = req.body;
+
+        const video = await Video.findOne({ filename });
+
+        res.json({ totalLikes: video.likes.length });
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
+    }
+
+
+})
+
+router.get('/getvideos', async (req, res) => {
+    try {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+
+        let videos = await Video.find();
+        res.json(videos);
+    } catch (error) {
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
+    }
+
+})
+
+router.post('/getuservideos', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    let videos = await Video.find({ author: req.body.username });
     res.json(videos);
 
 })
 
-router.post('/getuservideos', async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
-    let videos = await Video.find({author: req.body.username});
-    res.json(videos);
+router.post('/placebid', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
-})
-
-router.post('/placebid',fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
     try {
-        const {filename, bidamount, bidequity} = req.body;
+        const { filename, bidamount, bidequity } = req.body;
         const bidplacer = req.user.name;
-        await Video.findOneAndUpdate({filename: filename}, 
-            {$push: {
-                "bids": {amount: bidamount, equity: bidequity, bidplacer}
-            }});
-        const video = await Video.findOne({filename});
+        await Video.findOneAndUpdate({ filename: filename },
+            {
+                $push: {
+                    "bids": { amount: bidamount, equity: bidequity, bidplacer }
+                }
+            });
+        const video = await Video.findOne({ filename });
         console.log(video.bids);
-        res.json({bids: video.bids})
+        res.json({ bids: video.bids })
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 
 })
 
-router.post('/getbids',fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
+router.post('/getbids', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
     try {
 
-        const video = await Video.findOne({filename: req.body.filename});
-        res.json({bids: video.bids});
+        const video = await Video.findOne({ filename: req.body.filename });
+        res.json({ bids: video.bids });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 
 })
 
-router.post('/alreadysaved', fetchuser, async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
+router.post('/alreadysaved', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
     try {
         // const isLiked = await Video.findOne({filename: filename, likes: {username:req.user.username} })
 
-        const isSaved = await User.findOne({username: req.user.username})
-        for (var i=0; i<isSaved.saved.length; i++){
+        const isSaved = await User.findOne({ username: req.user.username })
+        for (var i = 0; i < isSaved.saved.length; i++) {
             // console.log(i, isSaved.saved[i].filename);
-            if(isSaved.saved[i].filename === req.body.filename){
-                return res.json({saved: true})
+            if (isSaved.saved[i].filename === req.body.filename) {
+                return res.json({ saved: true })
             }
         }
-        res.json({saved: false});
+        res.json({ saved: false });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 
 })
 
-router.post('/save', fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+router.post('/save', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     try {
 
-        const video = await Video.findOne({filename: req.body.filename});
+        const video = await Video.findOne({ filename: req.body.filename });
 
-        await User.findOneAndUpdate({username: req.user.username},
-            {$push: {
-                "saved": video
-            }});
+        await User.findOneAndUpdate({ username: req.user.username },
+            {
+                $push: {
+                    "saved": video
+                }
+            });
 
-        res.json({video:"saved"});
-        
-    } catch (error) {
-        console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
-    }
-})
-router.post('/unsave', fetchuser, async (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
-    
-    try {
-        const video = await Video.findOne({filename: req.body.filename});
-        await User.findOneAndUpdate({username: req.user.username}, 
-            {$pull: {
-                "saved": {filename: video.filename}
-            }});
-        res.json({success: true})
+        res.json({ video: "saved" });
 
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
-
 })
-
-router.get('/saved', fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+router.post('/unsave', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     try {
+        const video = await Video.findOne({ filename: req.body.filename });
+        await User.findOneAndUpdate({ username: req.user.username },
+            {
+                $pull: {
+                    "saved": { filename: video.filename }
+                }
+            });
+        res.json({ success: true })
 
-        const user = await User.findOne({username: req.user.username})
-        res.json({saved: user.saved});
-        
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
+
 })
 
-router.post('/comment', fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+router.get('/saved', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     try {
-        
-        await Video.findOneAndUpdate({filename: req.body.filename},
-            {$push: {
-                "comments": {username: req.user.username, comment: req.body.comment}
-            }});
 
-        res.json({success: true});
-        
+        const user = await User.findOne({ username: req.user.username })
+        res.json({ saved: user.saved });
+
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 })
 
-router.post('/getcomments', fetchuser, async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+router.post('/comment', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     try {
-        const video = await Video.findOne({filename: req.body.filename})
-        res.json({comments: video.comments});
-        
+
+        await Video.findOneAndUpdate({ filename: req.body.filename },
+            {
+                $push: {
+                    "comments": { username: req.user.username, comment: req.body.comment }
+                }
+            });
+
+        res.json({ success: true });
+
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 })
 
-router.post('/getpostbyname', async(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+router.post('/getcomments', fetchuser, async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     try {
-        const video = await Video.findOne({filename: req.body.filename})
+        const video = await Video.findOne({ filename: req.body.filename })
+        res.json({ comments: video.comments });
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
+    }
+})
+
+router.post('/getpostbyname', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    try {
+        const video = await Video.findOne({ filename: req.body.filename })
         res.json(video);
-        
+
     } catch (error) {
         console.error(error.message);
-        return res.status(400).json({success: false, error: "Internal Server Error"});
+        return res.status(400).json({ success: false, error: "Internal Server Error" });
     }
 })
 
