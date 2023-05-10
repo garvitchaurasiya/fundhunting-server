4f37b97bc80beae049186c7869861e8e57981ef1
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2023-05-10 09:22:11 +0530

    Created environment variables

--- database/db.js
+++ database/db.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
-const mongoDB_URI = "mongodb+srv://garvit:20020725@cluster0.nhpd38n.mongodb.net/?retryWrites=true&w=majority";
-// const mongoDB_URI = process.env.DATABASE;
-// console.log(process.env.DATABASE);
-// const mongoDB_URI = "mongodb://localhost:27017/fundhunting";
-
 const connectToMongoDB = ()=>{
-    mongoose.connect(mongoDB_URI, ()=>{
+    mongoose.connect(process.env.MONGODB_URI, ()=>{
         console.log("Connected to MongoDB successfully");
     })
 }
